0d0fe246942ba0f75fe2b0e9402db33fbfb4a456
Rocco Melzian <rocco@melzian.eu>
2022-06-26 11:26:59 +0200

    Kleiner Fehler in Musterloesung

--- projects/MMOG/client/1_rettung_bei_x_y/client_rettung_bei_x_y_musterloesung.py
+++ projects/MMOG/client/1_rettung_bei_x_y/client_rettung_bei_x_y_musterloesung.py
@@ -55,67 +55,67 @@ def rettung_bei_x_y(game_client, x_ziel = 0, y_ziel = 0):
     """
     # zunächst einmal die Attribute aktualisieren, damit wir wissen, wo wir stehen.
     game_client.publish("status")
-    
+
     # geplaner Algorithmus:
     # solange, bis man im Ziel 0,0 ist: (ausser wenn wir hoffnungslos feststecken, dann neu spawnen und neu versuchen)
-    # - erst so weit wie moeglich nach nord_west, 
-    # - dann so weit wie moeglich nach nord, 
+    # - erst so weit wie moeglich nach nord_west,
+    # - dann so weit wie moeglich nach nord,
     # - dann sued_west,
     # - dann wieder so weit wie moeglich nach nord_west usw.
-    
+
     # damit wir wissen, wann wir neu spawnen muessen
     hoffnung = 3
-    
+
     # die Reihenfolge der Richtungen, die wir laufen, bis wir feststecken
-    
-    # ToDo: dieser Algorithmus klappt nicht immer, da die Funktion 
-    # einen_schritt_nach() ist nicht perfekt ist, da man immer noch im 
+
+    # ToDo: dieser Algorithmus klappt nicht immer, da die Funktion
+    # einen_schritt_nach() ist nicht perfekt ist, da man immer noch im
     # Osten und Sueden von der Spielfläche fallen kann.
-    richtungen = ("nord_west", "nord", "sued_west") 
-    
+    richtungen = ("nord_west", "nord", "sued_west")
+
     # aktueller Standort
     attribute = game_client.attribute()
     x_aktuell, y_aktuell = attribute["position"]
-    
+
     ziel_erreicht = x_aktuell == x_ziel and y_aktuell == y_ziel
-    
+
     while not ziel_erreicht:
         for richtung in richtungen: # so lange in eine Richtung laufen, bis wir feststecken
 
             # aktueller Standort
             attribute = game_client.attribute()
             x_aktuell, y_aktuell = attribute["position"]
-            
+
             stecke_fest = False
-            while not stecke_fest:               
+            while not stecke_fest:
                 # x_alt, y_alt werden nach dem move verwendet, um festzustellen, ob wir feststecken.
                 x_alt, y_alt = x_aktuell, y_aktuell
-                
+
                 # jetzt in die aktuelle Richtung laufen
                 einen_schritt_nach(game_client, richtung)
 
                 # wo stehen wir nach dem Move?
                 attribute = game_client.attribute()
                 x_aktuell, y_aktuell = attribute["position"]
-                
+
                 # Abbruchbedingung der while-schleife aktualisieren
                 stecke_fest = x_alt == x_aktuell and y_alt == y_aktuell
-            
+
                 # damit wir auf der Konsole sehen, was los ist ...
                 game_client.print_attribute()
-                
+
             # bevor wir in eine neue Richtung laufen, erstmal schauen, ob wir nicht doch schon am Ziel sind und den for-loop verlassen koennen.
             ziel_erreicht = x_aktuell == x_ziel and y_aktuell == y_ziel
             if ziel_erreicht:
                 break
-            
+
         # Falls wir hoffnungslos feststecken, brechen wir ab, re-spawnen und probieren es nochmal von der neuen Position aus.
         hoffnung -= 1
-        if hoffnung < 1:
+        if hoffnung < 1 and not ziel_erreicht:
             game_client.publish("spawn")
             hoffnung = 3
 
-        
+
 ##############################################################
 # Programm
 ##############################################################
